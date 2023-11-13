b2544306e1d78f9879497791eea9c6d53da76591
KonstantinovArtur <akonstantinov538@gmail.com>
2023-11-13 13:17:06 +0300

    Проводник

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проводник
+{
+        internal class SR_menu
+        {
+            public int maxstrel;
+            public int minstrel;
+
+
+        public int sr()
+        {
+            ConsoleKeyInfo key;
+            key = Console.ReadKey();
+            int pos = 1;
+
+            while (key.Key != ConsoleKey.Enter)
+            {
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("  ");
+                if (key.Key == ConsoleKey.UpArrow && pos != 0)
+                {
+                    pos--;
+                }
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    pos++;
+                }
+                else if (key.Key == ConsoleKey.Escape)
+                {
+                    return -1;
+                }
+
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("->");
+
+                key = Console.ReadKey();
+            }
+            return pos;
+        }
+    }
+    
+}
+
